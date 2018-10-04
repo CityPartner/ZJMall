8ab947e3956ca2ece5d99a4997db6c41b6b05abd
DDLD <747413073@qq.com>
2018-10-04 13:44:06 +0800

    2018.10.4 13:43

--- src/main/java/com/nchhr/mall/MallApplication.java
+++ src/main/java/com/nchhr/mall/MallApplication.java
@@ -3,12 +3,20 @@ package com.nchhr.mall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 @MapperScan("com.nchhr.mall.dao")
-public class MallApplication {
+public class MallApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MallApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MallApplication.class);
     }
 }
