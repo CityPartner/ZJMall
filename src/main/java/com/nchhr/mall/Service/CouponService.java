955a48e6dd5560ade3e9a40685d5d9889a766c54
Jacy <1399739901@qq.com>
2018-10-06 11:37:16 +0800

    Jacy 2010/10/06

--- src/main/java/com/nchhr/mall/Service/CouponService.java
+++ src/main/java/com/nchhr/mall/Service/CouponService.java
@@ -1,4 +1,19 @@
 package com.nchhr.mall.service;
 
+import com.nchhr.mall.dao.CouponDao;
+import com.nchhr.mall.entity.CouponEntity;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
 public class CouponService {
+
+    @Resource
+    private CouponDao couponDao;
+
+    public List<CouponEntity> getCoupons(String userId, String state) {
+        return couponDao.getCoupons(userId, state);
+    }
 }
