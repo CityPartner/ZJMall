7361c5fbede4fcb40d4868b5f3116747660edd0c
dogbaha <HWGEAD@gmail.com>
2018-10-07 15:30:18 +0800

    样式更新--HWG

--- src/main/resources/static/js/pro_info.js
+++ src/main/resources/static/js/pro_info.js
@@ -23,7 +23,7 @@ $(function(){
         // alert(C_id+"  "+BuyCount);
         $("#selcet_cart").removeClass("weui-popup__container--visible");
         $.ajax({
-            url:"/mall/addToCart",
+            url:"/mall/shopCart/addToCart",
             data:"BuyCount="+BuyCount+"&C_id="+C_id,
             type:"post",
             success:showAddCartResult,
@@ -35,12 +35,57 @@ $(function(){
     $("#BuyNowBtn1").click(function () {
         $("#selcet_sku").addClass("weui-popup__container--visible");
     });
+    $("#BuyNowBtn2").click(function () {
+        // alert("hello");
+        var BuyCount=$("#BuyCount3").val();
+        var stock=$("#stock").val();
+        if(BuyCount == ""){
+            alert("请输入购买数量");
+            return ;
+        }else if(Number(BuyCount)>Number(stock)){
+            alert("最多只能购买"+stock+"箱");
+            return ;
+        }else if(Number(BuyCount)<1){
+            alert("请输入有效数字！");
+            return ;
+        }
+        var C_id=$("#C_id").val();
+        var dataString=C_id;
+        dataString += ":"+BuyCount+",";
+        window.location.href="/mall/saveCommodityData?CommodityData="+dataString;
+    });
     $("#NoISeeMore2,#CloseByDia").click(function () {
         $("#selcet_sku").removeClass("weui-popup__container--visible");
     });
     $("#CloseCartDia").click(function () {
         $("#selcet_cart").removeClass("weui-popup__container--visible");
     });
+
+    $(".DecreaseNum").click(function () {
+        // alert("hello");
+        var num=Number($(this).next().val());
+        // alert(num);
+        // alert("-1=="+num);
+        // num -= 1;
+        // alert(num);
+        if(Number(num)<=1){
+
+        }else{
+            $(this).next().prop("value",num-1);
+        }
+    });
+    $(".IncreaseNum").click(function () {
+        // alert("hello");
+        var num=Number($(this).prev().val());
+        // alert(num);
+        // alert("+1=="+num);
+        // num += 1;
+        // alert(num);
+        $(this).prev().prop("value",num+1);
+    });
+
+
+
 });
 
 function showAddCartResult(data) {
